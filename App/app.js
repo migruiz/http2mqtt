831c2274587c898f5e15c2e626d67328b3c51a6a
Sonia <soniab@timedatasecurity.com>
2021-03-28 21:55:26 +0100

    raw app.js

---
+++ App/app.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const mqtt = require('mqtt');
+
+http.createServer((request, response) => {
+  const { headers, method, url } = request;
+  let rawbody = [];
+  request.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    rawbody.push(chunk);
+  }).on('end', async () => {
+    rawbody = Buffer.concat(body).toString();
+    console.log("rawbody",rawbody)
+    const jsonData = JSON.parse(rawbody)
+    
+
+    const client  = mqtt.connect(process.env.MQTTLOCAL);
+    client.on('connect', function () {
+      console.log('entered');
+        client.publish(jsonData.topic,jsonData.message);
+        client.end();
+        response.end();
+    })
+    
+    
+    
+  });
+}).listen(80);
+
+
+
